2d53682d221a5890003c5a639fa3e4db9dcba04b
CLAY ___ IS   HERE‼ <shashanktiwari390@gmail.com>
2022-10-14 19:11:29 +0400

    shashank

--- lib/screens/animated_screen.dart
+++ lib/screens/animated_screen.dart
@@ -1,6 +1,5 @@
-import 'dart:math' as math;
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AnimatedScreenWidget extends StatefulWidget {
   final Widget child;
@@ -13,15 +12,18 @@ class AnimatedScreenWidget extends StatefulWidget {
 
 class _AnimatedScreenWidgetState extends State<AnimatedScreenWidget>
     with TickerProviderStateMixin {
+  final RxBool _isLoading = false.obs;
   late AnimationController _controller;
 
   @override
   void initState() {
+    _isLoading.value = true;
     super.initState();
     _controller = AnimationController(
       duration: const Duration(seconds: 4),
       vsync: this,
     )..repeat();
+    _isLoading.value = false;
   }
 
   @override
@@ -33,38 +35,40 @@ class _AnimatedScreenWidgetState extends State<AnimatedScreenWidget>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, _) {
-            return Transform.rotate(
-              angle: _controller.value * 2 * math.pi,
-              child: widget.child,
-            );
+      body: Obx(() {
+        return Center(
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, _) {
+              /*return Transform.rotate(
+                angle: _controller.value * 2 * math.pi,
+                child: widget.child,
+              );*/
 
-            return Container(
-              width: 200,
-              height: 200,
-              color: Colors.amber.shade400,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/shopping2.jpg'),
+              return Container(
+                width: 200,
+                height: 200,
+                color: Colors.amber.shade400,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/shopping2.jpg'),
+                        ),
+                        shape: BoxShape.rectangle,
                       ),
-                      shape: BoxShape.rectangle,
                     ),
-                  ),
-                  const SizedBox(height: 64),
-                  const Spacer(),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
+                    const SizedBox(height: 64),
+                    const Spacer(),
+                  ],
+                ),
+              );
+            },
+          ),
+        );
+      }),
     );
   }
 }
